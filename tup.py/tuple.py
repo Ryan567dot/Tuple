bff71bb523f3b29ffc5c650ec4fb907bd83b9c84
Ryan567dot <itsbluey1010@gmail.com>
2024-09-22 21:44:32 +0600

    tuple.py

---
+++ tup.py/tuple.py
@@ -0,0 +1,24 @@
+list = ["Zaheen","Atiya","Ryan","Tahmid"]
+
+frinds = ("Zaheen","Atiya","Ryan","Tahmid")
+
+#Access an Element
+
+print(frinds[2])
+print(frinds[0:3])
+
+print(len(frind))
+
+#Iteration
+
+for name in frinds:
+    print(name)
+    
+#Concatenation and Multiplication
+
+n1 = (1,2,3)
+n2 = (4,5,6)
+
+#Membership Operation
+
+print("Tahmid" in frinds)
